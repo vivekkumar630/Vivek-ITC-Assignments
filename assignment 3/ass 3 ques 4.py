43172f9ebc9b374e56f186d63d8c80ae072a663e
vivekkumar630 <121219188+vivekkumar630@users.noreply.github.com>
2022-12-22 16:17:22 +0530

    Update ass 3 ques 4.py

--- assignment 3/ass 3 ques 4.py
+++ assignment 3/ass 3 ques 4.py
@@ -1,14 +1,19 @@
-grade_point=int(input("Enter grade points:"))
-dic={10:"Your grade is 'A+' and Outstanding performance.",
-     9:"Your grade is 'A' and Excellent performance.",
-     8:"Your grade is 'B+' and Very Good performance.",
-     7:"Your grade is 'B' and Good performance.",
-     6:"Your grade is 'C+' and Average performance.",
-     5:"Your grade is 'C' and Below Average performance.",
-     4:"Your grade is 'D' and Poor performance."}
-     
-if 4<=grade_point<=10:
-        statement=dic.get(grade_point)
-        print(statement)
-else:
-       print("\nError\nPlease enter grade in range [4,10]")
+ a=float(input("Enter Grade points to be evaluated: "))
+
+if 9<a<=10:
+    print("Your Grade is 'A+' and Outstanding performance")
+elif 8<a<=9:
+    print("Your Grade is 'A' and Excellent performance")
+elif 7<a<=8:
+    print("Your Grade is 'B+' and Very Good performance")
+elif 6<a<=7:
+    print("Your Grade is 'B' and Good performance")    
+elif 5<a<=6:
+    print("Your Grade is 'C+' and Average performance")
+elif 4<a<=5:
+    print("Your Grade is 'C' and Below Average performance")
+elif a==4:
+    print("Your Grade is 'D' and Poor performance")
+if a<4 or a>10:
+    print("Error Enter a Grade between 0-10 only")
+    exit()
